b3d9f84bd676f8a7fcf8747ccfa2dbf860218d38
morpho_deidamia <vika.serg.karap@gmail.com>
2025-04-09 02:01:16 +0300

    article customization functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, useState, CSSProperties } from 'react';
 import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
+import { defaultArticleState, ArticleStateType } from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
@@ -13,26 +11,40 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
-			<Article />
-		</div>
-	);
+  const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
+
+  const applyStyles = (newState: ArticleStateType) => {
+    setArticleState(newState);
+    const rootStyle = document.documentElement.style;
+    rootStyle.setProperty('--font-family', newState.fontFamilyOption.value);
+    rootStyle.setProperty('--font-size', newState.fontSizeOption.value);
+    rootStyle.setProperty('--font-color', newState.fontColor.value);
+    rootStyle.setProperty('--bg-color', newState.backgroundColor.value);
+    rootStyle.setProperty('--container-width', newState.contentWidth.value);
+  };
+
+  return (
+    <div 
+      className={clsx(styles.main)}
+      style={{
+        '--font-family': articleState.fontFamilyOption.value,
+        '--font-size': articleState.fontSizeOption.value,
+        '--font-color': articleState.fontColor.value,
+        '--bg-color': articleState.backgroundColor.value,
+        '--container-width': articleState.contentWidth.value,
+      } as CSSProperties}
+    >
+      <ArticleParamsForm 
+        defaultState={defaultArticleState}
+        onChange={applyStyles} 
+      />
+      <Article />
+    </div>
+  );
 };
 
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
